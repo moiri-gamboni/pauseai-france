e54e86002d6ac322b727cee7be61d8292ee5729c
Orion1024 <orion1024@users.noreply.github.com>
2024-09-11 16:23:26 +0200

    Responsive width
    1ere version
    
    Add iframe title

--- src/lib/components/home/newsletter.svelte
+++ src/lib/components/home/newsletter.svelte
@@ -1,20 +1,44 @@
 <script lang="ts">
 	import UnderlinedTitle from '$components/UnderlinedTitle.svelte'
-	
 	const label_id = 'newsletter-title'
 </script>
 
 <section aria-labelledby={label_id}>
-	<UnderlinedTitle id={label_id}>Inscription à notre newsletter</UnderlinedTitle>
-	<div class="embedded-substack">
-		<iframe src="https://pauseia.substack.com/embed" width="480" height="320" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
+	<div class="newsletter-section">
+		<UnderlinedTitle id={label_id}>Inscription à notre newsletter</UnderlinedTitle>
+		<div class="embedded-substack">
+			<iframe
+				title="embedded-substack"
+				src="https://pauseia.substack.com/embed"
+				frameborder="0"
+				scrolling="no"
+			></iframe>
+		</div>
 	</div>
 </section>
 
-
 <style>
-
-	.embedded-substack {
+	.newsletter-section {
 		text-align: center;
+		align-items: center;
+	}
+	.embedded-substack {
+		width: 480px;
+		height: 320px;
+		margin: auto;
+	}
+
+	.embedded-substack iframe {
+		/* border:1px solid #000000; */
+		border: 1px solid var(--brand);
+		background: white;
+		width: 100%;
+		height: 100%;
+	}
+
+	@media (max-width: 550px) {
+		.embedded-substack {
+			width: 90%;
+		}
 	}
 </style>
